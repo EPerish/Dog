4804e26b8ad2c186140175e492fd72545e98543d
EPerish <persheva@mail.ru>
2023-05-07 16:25:59 +0300

    исправлены стили для Лого

--- src/layout/Logo.jsx
+++ src/layout/Logo.jsx
@@ -1,7 +1,8 @@
 import image from "../assets/logo.png";
+import styles from "./Header/header.module.css"
 
 const Logo = () => {
-    return <a href="/" className="logo img">
+    return <a href="/" className={styles.logo_a}>
         <img src={image} alt="DogFood"/>
         <span>CatDog</span>
     </a>
